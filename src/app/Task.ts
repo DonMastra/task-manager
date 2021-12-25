f7e3c2464b38e0c5f35dd33dd6c34b8f9c6b9e0f
Andres Mastracchio <andres.mastracchio@naranjax.com>
2021-12-25 17:25:39 -0300

    added Task interface and mocked tasks

---
+++ src/app/Task.ts
@@ -0,0 +1,8 @@
+export interface Task {
+
+  id: number;
+  text: String;
+  day: String;
+  reminder: boolean;
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task";
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Terminar 1er módulo de Angular",
+    day: "Diciembre 26 a las 12:00",
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Hacer la compras",
+    day: "Diciembre 26 a las 14:00",
+    reminder: true
+  },
+  {
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: "Diciembre 26 a las 17:00",
+    reminder: false
+  },
+  {
+    id: 4,
+    text: "Leer un rato",
+    day: "Diciembre 26 a las 20:00",
+    reminder: false
+  }
+]
