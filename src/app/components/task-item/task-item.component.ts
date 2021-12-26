fdf28ab2e901d5d4f5a22928b968793e04c3f015
Andres Mastracchio <andres.mastracchio@naranjax.com>
2021-12-26 20:19:58 -0300

    added functions to delete task and toggle reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-tasks';
@@ -14,11 +14,25 @@ export class TaskItemComponent implements OnInit {
   @Input()
   task: Task = TASKS[0];
 
+  @Output()
+  onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
+  @Output()
+  onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: Task): void {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 
 }
